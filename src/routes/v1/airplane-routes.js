549860e5ee7b2abed8cd150c183ca6953b587973
Bhavanshu Jain <jain.bhavanshu1112@gmail.com>
2023-08-16 11:17:40 +0530

    Added getAllAirplanes API

--- src/routes/v1/airplane-routes.js
+++ src/routes/v1/airplane-routes.js
@@ -8,4 +8,9 @@ const router = express.Router();
 // /api/v1/airplanes  POST request
 router.post('/', AirplaneMiddlewares.validateCreateRequest, Airplanecontroller.createAirplane);
 
+
+// /api/v1/airplanes  GET request
+router.get('/',  Airplanecontroller.getAirplanes);
+
 module.exports = router;
+
